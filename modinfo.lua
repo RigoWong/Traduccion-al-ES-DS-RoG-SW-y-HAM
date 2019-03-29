ca663ffe016356d188d64f265226fbf672a48227
Rodrigo Perez <r.igin@live.com.mx>
2019-03-29 13:16:23 -0600

    Traducción al 0%

---
+++ modinfo.lua
@@ -0,0 +1,22 @@
+-- This information tells other players more about the mod
+name = "Traducción Completa al Español"
+description = "Esta Traduccion Incluyen RG,SW y H."
+author = "Rigo Wong y Zombyx"
+version = "1.0"
+
+-- This is the URL name of the mod's thread on the forum; the part after the index.php? and before the first & in the URL
+-- Example:
+-- http://forums.kleientertainment.com/index.php?/files/file/202-sample-mods/
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 4
+
+-- Can specify a custom icon for this mod!
+--icon_atlas = "modicon.xml"
+--icon = "modicon.tex"
+
+--Compatible with both the base game and Reign of Giants
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = true
+hamlet_compatible = true
