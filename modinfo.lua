b4e303e34dc9f268f57debbf532d974fa629adf5
Rodrigo Perez <r.igin@live.com.mx>
2019-04-20 21:55:12 -0500

    Traducción al 37%

--- modinfo.lua
+++ modinfo.lua
@@ -1,23 +1,24 @@
 -- This information tells other players more about the mod
 name = "Hamlet en Español"
-description = "Esta Traduccion contendra RoG,SW y HAM."
+description = "Esta Traduccion contiene RoG, SW y HAM."
 author = "RigoWon y Zombyx"
-version = "1.9.1"
+version = "1.10.1"
 
 -- This is the URL name of the mod's thread on the forum; the part after the index.php? and before the first & in the URL
 -- Example:
 -- http://forums.kleientertainment.com/index.php?/files/file/202-sample-mods/
 -- becomes
 -- /files/file/202-sample-mods/
---forumthread = "/files/file/202-sample-mods/"
+forumthread = "/files/file/202-sample-mods/"
 
 -- This lets other players know if your mod is out of date, update it to match the current version in the game
-api_version = 5
+api_version = 6
 
 -- Can specify a custom icon for this mod!
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 --Compatible with both the base game and Reign of Giants
+dst_compatible = false 
 dont_starve_compatible = true
 reign_of_giants_compatible = true
 shipwrecked_compatible = true
